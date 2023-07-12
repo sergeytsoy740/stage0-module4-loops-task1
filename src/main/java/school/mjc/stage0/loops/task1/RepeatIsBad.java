9d2d80befaf8b52e8a9dcff143db64f2eb8e4f1e
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-12 12:04:03 +0600

    Module #4.2. Introduction. Task 1: Repeat Is Bad.(4)

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,11 +3,9 @@ package school.mjc.stage0.loops.task1;
 import java.util.stream.Stream;
 
 public class RepeatIsBad {
-    public static void main(String[] args) {
-        repeatIsBad();
-    }
+
     @SuppressWarnings("java:S106")
-    public static void repeatIsBad(){
+    public void repeatIsBad(){
       //  System.out.println("writing the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming");
 
         Stream
