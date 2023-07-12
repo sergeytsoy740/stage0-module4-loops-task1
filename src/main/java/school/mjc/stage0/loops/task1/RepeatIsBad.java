623c008db3276c12d3da8f0ba7a39756d82c06ee
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-12 12:00:53 +0600

    Module #4.2. Introduction. Task 1: Repeat Is Bad.(3)

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,11 +1,18 @@
 package school.mjc.stage0.loops.task1;
 
+import java.util.stream.Stream;
+
 public class RepeatIsBad {
-
+    public static void main(String[] args) {
+        repeatIsBad();
+    }
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-        System.out.println("writing the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming");
+    public static void repeatIsBad(){
+      //  System.out.println("writing the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming\nwriting the same code doesn't have much impact, and it's also time consuming");
 
-
+        Stream
+                .generate(() -> "writing the same code doesn't have much impact, and it's also time consuming")
+                .limit(20)
+                .forEach(System.out::println);
     }
 }
